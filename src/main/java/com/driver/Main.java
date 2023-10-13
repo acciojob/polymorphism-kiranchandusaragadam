6273e0a0e3837410b585828ed5bf9cba02c40542
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-10-13 23:12:24 +0530

    polymorphism done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,34 @@
 package com.driver;
 
 public class Main {
+    public Main(){
 
+    }
+
+    public static class Product{
+        public Product(){
+
+        }
+
+        public int product(int x, int y){
+            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return (double)(x*y);
+        }
+
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        int s1 = p.product(3, 2);
+        int s2 = p.product(4, 3, 2);
+        double s3 = p.product(3.0, 2.5);
+
+        System.out.println(s1 + " " + s2 + " " + s3);
+    }
 }
